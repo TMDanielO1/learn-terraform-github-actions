3669c310d17d23fa429a232779d3daf0a2748df4
root <root@rhel74foraws.danielvm.com>
2020-12-08 11:21:31 +0800

    revert changing the web server tag

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ provider "random" {}
 
 resource "random_pet" "sg" {}
 
-resource "aws_instance" "web-test" {
+resource "aws_instance" "web" {
   ami                    = "ami-09558250a3419e7d0"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
